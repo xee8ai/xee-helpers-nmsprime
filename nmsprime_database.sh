1ff43ae8bbf14177008d778819811d1f83d8ae0f
xee8ai <xee8ai@users.noreply.github.com>
2020-01-15 18:16:53 +0100

    Add database cacti to access script.

--- nmsprime_database.sh
+++ nmsprime_database.sh
@@ -6,6 +6,10 @@ if [[ "$1" == ccc ]]; then
         USER=$(python $SCRIPT_DIR/nmsprime_get_env.py user_ccc local)
         PASSWD=$(python $SCRIPT_DIR/nmsprime_get_env.py password_ccc local)
         DB=$(python $SCRIPT_DIR/nmsprime_get_env.py db_ccc local)
+elif [[ "$1" == cacti ]]; then
+        USER=$(python $SCRIPT_DIR/nmsprime_get_env.py user_cacti local)
+        PASSWD=$(python $SCRIPT_DIR/nmsprime_get_env.py password_cacti local)
+        DB=$(python $SCRIPT_DIR/nmsprime_get_env.py db_cacti local)
 else
         USER=$(python $SCRIPT_DIR/nmsprime_get_env.py user local)
         PASSWD=$(python $SCRIPT_DIR/nmsprime_get_env.py password local)
