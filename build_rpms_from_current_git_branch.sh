a33567e97387742ff365e8b337f44d4b6b347967
xee8ai <xee8ai@users.noreply.github.com>
2020-01-15 10:06:18 +0100

    Add script to build rpms from current branch.

---
+++ build_rpms_from_current_git_branch.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# fpm has to be installed:
+#	https://github.com/jordansissel/fpm
+#	https://fpm.readthedocs.io/en/latest/installing.html
+#
+#	yum install ruby-devel gcc make rpm-build rubygems
+#	gem install --no-document fpm
+
+SRC="/var/www/nmsprime"
+DST="/var/www/rpm/nmsprime"
+FPM="/usr/local/bin/fpm"
+
+cd "$SRC"
+
+echo
+echo "Getting tags…"
+git fetch -tp
+echo "Done…"
+
+echo
+echo "There are the following tags in this repo"
+git tag
+
+echo
+read -p "Which RPM version you want to build (e.g. 2.4.1)? " RPMVERSION
+
+DST="$DST/$RPMVERSION"
+mkdir -p "$DST"
+
+echo
+echo "Running “composer update”…"
+composer update
+echo "Done…"
+
+echo
+echo "Creating RPMs…"
+php Install/install.php "$RPMVERSION" . "$DST"
+echo "Done…"
+echo "RPMs went to $DST:"
+ls -l "$DST"
+echo
